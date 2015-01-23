dc29dd54dce1d7b97e36ad99b76cb973dfbf91f3
pjwl33 <paul.leejw@gmail.com>
2015-01-23 15:43:40 -0500

    routes files added

--- app.js
+++ app.js
@@ -13,65 +13,20 @@ var express = require('express'),
   app = express(),
   cons = require('consolidate'),
   MongoClient = require('mongodb').MongoClient,
-  Server = require('mongodb').Server;
+  routes = require('./routes');
 
-app.engine('html', cons.swig);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
-var mongoclient = new MongoClient(new Server('localhost', 27017, {
-  'native_parser': true
-}));
-
-var db = mongoclient.db('course');
-
-app.get('/', function(req, res) {
-  // res.send('Hello, World!');
-  db.collection('hello_mongo_express').findOne({}, function(err, doc) {
-    console.log(doc);
-    res.render('hello', doc);
-  });
-  // res.render('hello', {
-  //   'name': "Swig"
-  // });
-});
-
-app.get('/:name', function(req, res, next){
-  var name = req.params.name;
-  var getvar1 = req.query.getvar1 || '';
-  var getvar2 = req.query.getvar2 || '';
-  res.render('hello', {
-    'name': name,
-    'getvar1': getvar1,
-    'getvar2': getvar2
-  });
-});
-
-app.get('*', function(req, res) {
-  res.send("Page Not Found", 404);
-});
-
-mongoclient.open(function(err, mongoclient) {
+MongoClient.connect('mongodb://localhost:27017/blog', function(err, db) {
+  "use strict";
   if (err) throw err;
-  app.listen(8080);
-  console.log('Express server started on port 8080');
+
+  app.engine('html', cons.swig);
+  app.set('view engine', 'html');
+  app.set('views', __dirname + "/views");
+
+  app.use(express.cookieParser());
+  app.use(express.bodyParse());
+
+  routes(app, db);
+  app.listen(3000);
+  console.log("Express server listening on port 3000");
 });
-
-
-
-// var MongoClient = mongodb.MongoClient;
-
-// MongoClient.connect('mongodb://localhost:27107/test', function(err, db) {
-//   if (err) throw err;
-
-//   db.collection('coll').findOne({}, function(err, doc) {
-//     if (err) throw err;
-
-//     // Print result
-//     console.dir(doc);
-
-//     db.close();
-//   });
-
-//   console.dir("Called findOne!");
-// });
--- others/notes.js
+++ others/notes.js
@@ -21,12 +21,71 @@ db.scores.find({type: "exam"}).sort({score: -1}).skip(50).limit(20)
 
 db.collection('grades').findOne(query, callback);
 
-SUPPORT: Thresh, Morgana, Braum
-JUNGLE: Reksai, Amumu, Skarner, Rammus
-TOP: Reksai, Malphite, Lissandra
+// SUPPORT: Thresh, Morgana, Braum
+// JUNGLE: Reksai, Vi, Rammus
+// TOP: Reksai, Malphite, Lissandra
 
-ADC: Caitlyn, Jinx
-MID: Lissandra
+// ADC: Caitlyn, Jinx
+// MID: Lissandra
+
+// app.engine('html', cons.swig);
+// app.set('view engine', 'html');
+// app.set('views', __dirname + '/views');
+
+// var mongoclient = new MongoClient(new Server('localhost', 27017, {
+//   'native_parser': true
+// }));
+
+// var db = mongoclient.db('course');
+
+// app.get('/', function(req, res) {
+//   // res.send('Hello, World!');
+//   db.collection('hello_mongo_express').findOne({}, function(err, doc) {
+//     console.log(doc);
+//     res.render('hello', doc);
+//   });
+//   // res.render('hello', {
+//   //   'name': "Swig"
+//   // });
+// });
+
+// app.get('/:name', function(req, res, next){
+//   var name = req.params.name;
+//   var getvar1 = req.query.getvar1 || '';
+//   var getvar2 = req.query.getvar2 || '';
+//   res.render('hello', {
+//     'name': name,
+//     'getvar1': getvar1,
+//     'getvar2': getvar2
+//   });
+// });
+
+// app.get('*', function(req, res) {
+//   res.send("Page Not Found", 404);
+// });
+
+// mongoclient.open(function(err, mongoclient) {
+//   if (err) throw err;
+//   app.listen(8080);
+//   console.log('Express server started on port 8080');
+// });
 
 
+
+// var MongoClient = mongodb.MongoClient;
+
+// MongoClient.connect('mongodb://localhost:27107/test', function(err, db) {
+//   if (err) throw err;
+
+//   db.collection('coll').findOne({}, function(err, doc) {
+//     if (err) throw err;
+
+//     // Print result
+//     console.dir(doc);
+
+//     db.close();
+//   });
+
+//   console.dir("Called findOne!");
+// });
 
